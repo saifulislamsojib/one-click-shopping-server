11665552473580e1c36d43f4e92a244a3772f207
Saiful Islam Sojib <saifulislam.sojib56@gmail.com>
2021-04-01 01:33:34 +0600

    half work is done

---
+++ README.md
@@ -0,0 +1,1 @@
+# one-click-shopping-server
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 
@@ -10,6 +12,55 @@ const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello One Click Shopping!');
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ernz8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  const productsCollection = client.db("one-click-shopping").collection("products");
+
+  const ordersCollection = client.db("one-click-shopping").collection("orders");
+  
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    productsCollection.insertOne(product)
+    .then(result => {
+      res.send({inserted:result.insertedCount > 0, _id: result.insertedId});
+    })
+  });
+
+  app.get("/products", (req, res) => {
+    productsCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    });
+  });
+
+  app.get("/product/:id", (req, res) => {
+    const {id} = req.params;
+    productsCollection.find({_id: ObjectId(id)})
+    .toArray((err, documents) => {
+      res.send(documents[0])
+    });
+  });
+
+  app.post('/checkoutOrder', (req, res) => {
+    const orderedProduct = req.body;
+    ordersCollection.insertOne(orderedProduct)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    })
+  });
+
+  app.get("/orders/:email", (req, res) => {
+    const {email} = req.params;
+    ordersCollection.find({email})
+    .toArray((err, documents) => {
+      res.send(documents)
+    });
+  });
+
 });
 
 app.listen(port, () => {
